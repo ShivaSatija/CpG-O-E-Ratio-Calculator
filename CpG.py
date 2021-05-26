31dde05212375ad38d42d9e368f20c65dd77f75d
Shiva Satija <53440035+shiva-satija@users.noreply.github.com>
2021-05-26 23:26:21 +0530

    Update CpG.py

--- CpG.py
+++ CpG.py
@@ -8,13 +8,12 @@ A = [i.count("A") for i in DNA_seq]
 T = [i.count("T") for i in DNA_seq]
 G = [i.count("G") for i in DNA_seq]
 C = [i.count("C") for i in DNA_seq]
-CG = [i.count("CG") for i in DNA_seq]
-AllNul_dict = {"A":A, "T":T, "C":C, "G":G, "CG":CG, "seq_id":DNA_IDs, "Length":Length}
+CpG = [i.count("CG") for i in DNA_seq]
+AllNul_dict = {"A":A, "T":T, "C":C, "G":G, "CpG":CpG, "seq_id":DNA_IDs, "Length":Length}
 import pandas as pd
 df = pd.DataFrame(AllNul_dict)
-df["CpG"] = df.CG
-df["C_G"] = (df.C*df.G)
-df ["CpG_OE"] = (df.CpG*df.Length)/(df["C_G"])
+df ["CpG O/E"] = round((df.CpG*df.Length)/(df.G*df.C),4)
+df ["gc content %"] = round((df.G+df.C)*100/(df.Length),4)
 df.to_csv("CpG_oe.csv")
 df = pd.read_csv("CpG_oe.csv")
 df.inplace = True
